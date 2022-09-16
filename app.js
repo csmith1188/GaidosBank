9eadebcc486e5a4284f4964e57ed559691b20598
Riley23062 <82524320+Riley23062@users.noreply.github.com>
2022-09-16 09:51:13 -0400

    Admin login

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const fs = require('fs');
 const session = require('express-session');
 const db = new sqlite3.Database('./gaidosBank.db', sqlite3.OPEN_READWRITE)
 
-const sql = `INSERT INTO users (uID, Name )
-                VALUES(?,?)`
-const view = `SELECT * FROM users`
+
+const view = `SELECT * FROM transactions WHERE amount = ?`
 
 
 app.set('view engine', 'ejs');
@@ -23,7 +22,50 @@ app.get('/', (req, res) =>{
   res.render('home');
 })
 
+app.get('/transaction', (req, res) =>{
+	res.render('trans');
+  })
 
+
+
+app.post('/transaction', (req, res) => {
+	let amount = req.body.amount;
+	let id = req.body.id;
+})
+
+app.get('/admin', (req, res) =>{
+	res.render('admin')
+})
+
+app.post('/adminLogin', (req, res) =>{
+	let username = req.body.name;
+	let password = req.body.password;
+
+  if (username && password) {
+	console.log(username);
+	console.log(password);
+	// Execute SQL query that'll select the account from the database based on the specified username and password
+		db.get(`SELECT * FROM admin WHERE username = ? AND password = ?`, [username, password], function(error, results) {
+			// If there is an issue with the query, output the error
+			console.log(results);
+			if (error) throw error;
+			// If the account exists
+			if (results) {
+				// Authenticate the user
+				req.session.loggedin = true;
+				req.session.username = username;
+				// Redirect to home page
+				res.send('Welcome to the admin hub');
+			} else {
+				res.send('Incorrect Username and/or Password!');
+			}			
+			res.end();
+		});
+	} else {
+		res.send('Please enter Username and Password!');
+		res.end();
+	}
+})
 app.post('/login',(req, res) =>{
   let username = req.body.name;
 	let password = req.body.id;
@@ -53,31 +95,16 @@ app.post('/login',(req, res) =>{
 
 
 app.get('/home', function(req, res) {
-<<<<<<< HEAD
-<<<<<<< HEAD
 	// If the user is loggedin
 	if (req.session.loggedin) {
 		// Output username
-		res.send('Welcome back, ' + req.session.username + '!');
+		res.render('homepage', {
+			username: req.session.username
+		});
 	} else {
 		// Not logged in
 		res.send('Please login to view this page!');
 	}
-=======
-=======
->>>>>>> c2985fa1776ced3f967ef31ed3f28981290d280d
-	     // If the user is loggedin
-if (req.session.loggedin) {
-	// Output username
-	res.render('homepage', {username: req.session.username})
-} else {
-	// Not logged in
-	res.send('Please login to view this page!');
-}
-<<<<<<< HEAD
->>>>>>> c2985fa1776ced3f967ef31ed3f28981290d280d
-=======
->>>>>>> c2985fa1776ced3f967ef31ed3f28981290d280d
 	res.end();
 });
 
