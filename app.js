7f271fb8dfeba81ffe0b348d3bb6b8a1e4c1b568
JZeigler <Jayso2023519@live.ytech.edu>
2023-01-26 13:59:53 -0500

    Scalped App.js
    got rid of obsolete or broken code in app.js so we can basically start with a blank canvas

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const app = express();
-const port = 3306
+const port = 3101
 const fs = require('fs');
 const session = require('express-session');
 const db = new sqlite3.Database('./gaidosBank.db', sqlite3.OPEN_READWRITE)
-const encryptpwd = require('encrypt-with-password');
 
 
 
@@ -28,104 +27,6 @@ app.get('/', (req, res) => {
 	res.render('home');
 })
 
-app.get('/transaction', (req, res) =>{
-	res.render('trans');
-  })
-
-
-
-app.post('/transaction', (req, res) => {
-	let amount = req.body.amount;
-	let id = req.body.id;
-})
-
-app.get('/admin', (req, res) =>{
-	res.render('admin')
-})
-
-app.post('/adminLogin', (req, res) =>){
-	let username = req.body.name;
-	let password = req.body.password;}
-
-  if (username && password) {
-	console.log(username);
-	console.log(password)};
-	// Execute SQL query that'll select the account from the database based on the specified username and password
-		db.get(`SELECT * FROM admin WHERE username = ? AND password = ?`, [username, password], function(error, results)) {
-app.get('/register', (req, res) => {
-	res.render('register')};
-)}
-
-app.post('/register', (req, res) => {
-	let studentid = req.body.studentID;
-	let studentname = req.body.studentName;
-	if (studentid && studentname) {
-		// Execute SQL query that'll push the new account from the nodejs to the database based on the specified studentname and studentid
-		db.get(sql, [studentid, studentname], function (error, results) {
-			// If there is an issue with the query, output the error
-			console.log(results);
-			if (error) throw error;
-			// If the account exists
-			if (results) {
-				// Authenticate the user
-				req.session.loggedin = true;
-				req.session.username = username;
-				// Redirect to home page
-				res.send('Welcome to the admin hub');
-			} else {
-				res.send('Incorrect Username and/or Password!');
-			}			
-			res.end();
-		});
-	} else {
-		res.send('Please enter Username and Password!');
-		res.end();
-	}
-})
-
-app.post('/login', (req, res) => {
-	let username = req.body.name;
-	let password = req.body.id;
-	const encrypted = encryptpwd.encrypt(username, password);
-	const decrypted = encryptpwd.decrypt(encrypted, password);
-	if (username && password) {
-		// Execute SQL query that'll select the account from the database based on the specified username and password
-		db.get(`SELECT * FROM users WHERE Name = ? AND uID = ?`, [username, password], function (error, results) {
-			// If there is an issue with the query, output the error
-			console.log(results);
-			if (error) throw error;
-			// If the account exists
-			if (results) {
-				// Authenticate the user
-				req.session.loggedin = true;
-				req.session.username = username;
-				console.log(encrypted);
-				// Redirect to home page
-				res.redirect('/home');
-			} else {
-				res.send('Incorrect Username and/or Password!');
-			}
-			res.end();
-		});
-	} else {
-		res.send('Please enter Username and Password!');
-		res.end();
-	}
-})
-
-
-app.get('/home', function (req, res) {
-	// If the user is loggedin
-	if (req.session.loggedin) {
-		// Output username
-		res.render('homepage', { username: req.session.username })
-	} else {
-		// Not logged in
-		res.send('Please login to view this page!');
-	}
-	res.end();
-});
-
 app.listen(port, (err) =>{
 	if (err) {
 	  console.error(err);
@@ -133,9 +34,3 @@ app.listen(port, (err) =>{
 	  console.log(`Running on port ${port}`);
 	}
   });
-  
-  
-  
-  //db.close((err) => {
-  //if(err) return console.error(err.message);
-  //})
